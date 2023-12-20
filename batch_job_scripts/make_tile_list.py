38b0bc15083f5fe3eafa92db1354d0e40a17efe6
Eikka12 <einariheinaro@gmail.com>
2023-12-20 15:39:57 +0200

    Removed the original content of the repository.

--- batch_job_scripts/make_tile_list.py
+++
@@ -1,8 +0,0 @@
-import os
-
-tiles = [t[:-4] for t in os.listdir('/scratch/project_2001325/mayrajan/data/labeled_tiles/labeled_tiles')]
-tiles = list(set(tiles))
-paths = [f'ibc-carbon-evo-data/nn-processed-tiles/{t}.tif' for t in tiles]
-with open('tile_list', 'w') as f:
-    for p in paths:
-        f.write(f'{p}\n')
